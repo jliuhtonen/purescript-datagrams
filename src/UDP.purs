c0452289393f9df91d87ef61f21f9340517e03e8
Janne Liuhtonen <janne.liuhtonen@reaktor.fi>
2015-09-20 19:27:24 +0300

    Implement SocketInfo type + show typeclass

--- src/UDP.purs
+++ src/UDP.purs
@@ -1,6 +1,16 @@
 module Datagram.UDP where
 
+import Prelude
+
 foreign import data Socket :: *
 foreign import data SOCKET :: !
-foreign import data SocketInfo :: *
+
+newtype SocketInfo = SocketInfo {
+    port :: Int,
+    address :: String,
+    family :: String
+}
+
+instance showSocketInfo :: Show SocketInfo where
+    show (SocketInfo { port: p, address: a, family: f }) = "port: " ++ show p ++ " address: " ++ show a ++ " family: " ++ show f
 
